a67e216c472954ae27cb7edcbdc3db668a88df31
Shahriar1212 <shahriarmoin.aust@gmail.com>
2023-05-20 11:59:40 +0600

    Project setup

---
+++ toys-house-client/src/Components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavigationBar = () => {
+    return (
+        <div>
+            navigation
+        </div>
+    );
+};
+
+export default NavigationBar;
---
+++ toys-house-client/src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavigationBar from '../Components/NavigationBar/NavigationBar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Components/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <NavigationBar></NavigationBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
