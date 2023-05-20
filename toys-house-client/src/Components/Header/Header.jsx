743e6b76f8f384959e165f723cf2aa3366df701a
Shahriar1212 <shahriarmoin.aust@gmail.com>
2023-05-20 12:45:50 +0600

    basic navbar

--- toys-house-client/src/Components/Header/Header.jsx
+++ toys-house-client/src/Components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ava from '../../assets/images/avater.jpg'
 
 const Header = () => {
     return (
@@ -23,27 +25,27 @@ const Header = () => {
                         <li><a>Item 3</a></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl">Toys House</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <a>
-                            Parent
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </a>
-                        <ul className="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    <li><Link>Home</Link></li>
+                    <li><Link>All Toys</Link></li>
+                    <li><Link>My Toys</Link></li>
+                    <li><Link>Add A Toy</Link></li>
+                    <li><Link>Blogs</Link></li>
+
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Get started</a>
+                <div className="avatar pr-3">
+                    <div className="w-12 rounded-full">
+                        <img src={ava} />
+                    </div>
+                </div>
+                <a className="btn">Login</a>
             </div>
+
         </div>
     );
 };
