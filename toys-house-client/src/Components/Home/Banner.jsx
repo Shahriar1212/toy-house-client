5d153290ea5bee41cb6d5e18c6289e4ce47c8259
Shahriar1212 <shahriarmoin.aust@gmail.com>
2023-05-21 23:28:29 +0600

    Update Banner.jsx

--- toys-house-client/src/Components/Home/Banner.jsx
+++ toys-house-client/src/Components/Home/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
                     <div className="grid lg:grid-cols-2 gap-12 flex items-center">
                         <div className="mt-12 lg:mt-0">
                             <h1 className="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">The best Toy <br /><span className="text-blue-600">for your children</span></h1>
-                            <Link className="inline-block px-7 py-3 mr-2 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">See All</Link>
+                            <Link to="/alltoys" className="inline-block px-7 py-3 mr-2 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">See All</Link>
                             <Link className="inline-block px-7 py-3 bg-transparent text-blue-600 font-medium text-sm leading-snug uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Learn more</Link>
                         </div>
                         <div className="mb-12 lg:mb-0">
